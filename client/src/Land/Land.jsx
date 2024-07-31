33aae4fe56cd34c1a6b12069bade609ec5ed07b8
Sughavan <sughavang@gmail.com>
2024-07-31 11:53:56 +0530

    first commit

---
+++ client/src/Land/Land.jsx
@@ -0,0 +1,72 @@
+import React from 'react'
+import './Land.css'
+function Land(){
+  return (
+    <div>
+        
+        <div className='nav_bar'>
+
+            <div class="logo">
+                <h1>Vote Online</h1>
+            </div>
+
+            <div className="items">
+
+                  <li className='item'>
+                   <a className='ite'> Home</a>
+                   </li>
+                  <li className='item'>
+                   <a className='ite'>VoteNow!</a>
+                   </li>
+                  <li className='item'>
+                   <a className='ite'>ELections</a>
+                   </li>
+                  <li className='item'>
+                   <a className='ite'>
+                    <img src="" alt="profile"/>
+                   </a>
+                   </li>
+               
+               
+            </div>
+               
+                
+               
+        </div>
+
+        <div className='hero'>
+
+
+        </div>
+
+
+        <div className='body_section'>
+            
+            <div className='live_voting'>
+
+           
+            </div>
+
+            <div className='upcoming_election'>
+
+                <div className='upcome'>
+
+                </div>
+
+            </div>
+
+
+        </div>
+
+
+         
+        
+        <div className='footer'>
+
+        </div>
+
+    </div>
+  )
+}
+
+export default Land
